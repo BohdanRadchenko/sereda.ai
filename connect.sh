e055ded5b822443b8d483078ffa847859ecdbd76
BohdanRadchenko <bradchenko@gmail.com>
2024-11-01 08:24:02 +0200

    added docker compose

--- connect.sh
+++ connect.sh
@@ -2,3 +2,6 @@ ssh root@134.209.246.53
 HdVhhZ*5nuG92ED
 
 cd sereda.ai
+
+see ssh gpg
+https://gist.github.com/paolocarrasco/18ca8fe6e63490ae1be23e84a7039374?permalink_comment_id=3767413#gistcomment-3767413
