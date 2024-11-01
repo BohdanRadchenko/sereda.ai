b327f31591adaaeed4e652e7926fbde2e3857f5a
BohdanRadchenko <bradchenko@gmail.com>
2024-11-01 09:07:49 +0200

    Updates

---
+++ assets/FAQPage-8vNIQNDo.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-UDf1RCAU.js";const r=()=>e.jsx("div",{children:e.jsx("h1",{children:"FAQ Page"})});export{r as default};
---
+++ assets/MainPage-B6fhzbbZ.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index-UDf1RCAU.js";const i=()=>a.jsx("div",{children:a.jsx("h1",{children:"Main Page"})});export{i as default};
