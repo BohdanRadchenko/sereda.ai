0d2dbadfc2c15f42f4f2bb72267f4aea55e07d03
BohdanRadchenko <bradchenko@gmail.com>
2024-11-01 07:32:46 +0200

    Updates

--- assets/EventsPage-BfLH8wW8.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-CEChX7mC.js";const t=()=>e.jsx("div",{children:e.jsx("h1",{children:"Events Page"})});export{t as default};
---
+++ assets/EventsPage-DFY2Y2z0.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-pxYbUK7C.js";const t=()=>e.jsx("div",{children:e.jsx("h1",{children:"Events Page"})});export{t as default};
