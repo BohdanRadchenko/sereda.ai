5ba7522fd0728a7451794207fcedfaa4f541b8b3
BohdanRadchenko <bradchenko@gmail.com>
2024-11-01 07:03:49 +0200

    try hash router deploy fix

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,8 @@ export default defineConfig({
 	server: {
 		port: 5100
 	},
-	base: "bohdanradchenko.github.io/sereda.ai",
+	// base: "bohdanradchenko.github.io/sereda.ai",
+	base: "BohdanRadchenko",
 	plugins: [
 		react(),
 		tsconfigPaths(),
